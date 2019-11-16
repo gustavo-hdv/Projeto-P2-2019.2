eb1033e88f81e925dc3aec991b8434e72f3ab1e9
Carlos Henrique <carlos.ribeiro@ccc.ufcg.edu.br>
2019-11-16 18:44:50 -0300

    conclui documentacao us10

--- src/comparators/AtividadeMaisAntiga.java
+++ src/comparators/AtividadeMaisAntiga.java
@@ -3,14 +3,29 @@ package comparators;
 import java.util.Comparator;
 import projeto.AtividadeMetodologica;
 
+/**
+ * Estrategia de comparacao baseada na ordem de cadastro das atividades em uma
+ * pesquisa. Essa estrategia so leva em consideracao atividades que tenham itens
+ * pendentes.
+ * 
+ * @author Carlos Ribeiro
+ *
+ */
 public class AtividadeMaisAntiga implements Comparator<AtividadeMetodologica> {
 
+	/**
+	 * Compara duas atividades baseado em sua ordem de cadastro dentro de uma
+	 * pesquisa.
+	 */
 	@Override
 	public int compare(AtividadeMetodologica atv1, AtividadeMetodologica atv2) {
-		if (atv1.contaItensPendentes() == 0) return 1;
-		if (atv2.contaItensPendentes() == 0) return -1;
+		if (atv1.contaItensPendentes() == 0)
+			return 1;
+		if (atv2.contaItensPendentes() == 0)
+			return -1;
 		String codigo1 = atv1.getCodigo();
 		String codigo2 = atv2.getCodigo();
-		return Integer.parseInt(codigo1.substring(1, codigo1.length())) - Integer.parseInt(codigo2.substring(1, codigo2.length()));
+		return Integer.parseInt(codigo1.substring(1, codigo1.length()))
+				- Integer.parseInt(codigo2.substring(1, codigo2.length()));
 	}
 }
--- src/comparators/AtividadeMenosPendencias.java
+++ src/comparators/AtividadeMenosPendencias.java
@@ -3,13 +3,28 @@ package comparators;
 import java.util.Comparator;
 import projeto.AtividadeMetodologica;
 
+/**
+ * Estrategia de comparacao baseada na quantidade de itens pendentes das
+ * atividades em uma pesquisa. Essa estrategia so leva em consideracao
+ * atividades que tenham itens pendentes. Alem disso, o desempate baseia-se na
+ * ordem de cadastro das atividades.
+ * 
+ * @author Carlos Ribeiro
+ *
+ */
 public class AtividadeMenosPendencias implements Comparator<AtividadeMetodologica> {
 
+	/**
+	 * Compara duas atividades baseado em suas quantidades de pendencias dentro de
+	 * uma pesquisa.
+	 */
 	@Override
 	public int compare(AtividadeMetodologica atv1, AtividadeMetodologica atv2) {
 		int res = atv1.contaItensPendentes() - atv2.contaItensPendentes();
-		if (atv1.contaItensPendentes() == 0) return 1;
-		if (atv2.contaItensPendentes() == 0) return -1;
+		if (atv1.contaItensPendentes() == 0)
+			return 1;
+		if (atv2.contaItensPendentes() == 0)
+			return -1;
 		if (res == 0) {
 			Comparator<AtividadeMetodologica> estrategia = new AtividadeMaisAntiga();
 			return estrategia.compare(atv1, atv2);
